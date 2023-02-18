c3c83dd94da8b98c0b05b6ccb1070226c8ce75c4
parvez1219 <parvezrahaman1219@gmail.com>
2023-02-18 12:31:24 +0530

    emails

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,6 +10,7 @@ public class Main {
         email.changePassword("Accio@123", "V12@");
         email.changePassword("Acio@123", "V12@v");
         email.changePassword("Accio@123", "V12@v123");
+        //System.out.println(email.getPassword());
 
         Gmail gmail = new Gmail("accio@gmail.com", 3);
         gmail.receiveMail(new SimpleDateFormat("dd/MM/yyyy").parse("21/12/2022"), "Tushar", "Assignment Completed?");
